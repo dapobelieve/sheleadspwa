c7918663596403d843f04b0c0ec131c4044cade5
Dapo Michaels <dapomichaels@gmail.com>
2020-04-02 12:24:40 +0100

    updates to nav

--- src/components/SlaMenu.vue
+++ src/components/SlaMenu.vue
@@ -1,11 +1,31 @@
 <template>
-  <div style="height: 56px;" class="d-flex align-items-center justify-content-between position-fixed width-100 bottom-0 z-index-1 bg-white shadow-3 mt-2">
-    <icon class="flex-grow-1 text-primary" size="lg" name="home"/>
-    <icon class="flex-grow-1 text-primary" size="lg" name="search"/>
-    <icon class="flex-grow-1 text-primary" size="lg" name="notification"/>
-    <icon class="flex-grow-1 text-primary" size="lg" name="leader"/>
-    <icon class="flex-grow-1 text-primary" size="lg" name="course"/>
-  </div>
+  	<div style="height: 56px;" class="d-flex align-items-baseline justify-content-around position-fixed width-100 bottom-0 z-index-1 bg-white shadow-3 px-4">
+	    <router-link to="/" class="flex-inline cursor-pointer text-grey flex-column align-items-center">
+	    	<icon size="lg" name="home"/>
+	    	<small class="text-align-center">Home</small>
+	    	<div class="indicator"></div>
+	    </router-link>
+	    <router-link to="/" class="flex-inline cursor-pointer text-grey flex-column align-items-center">
+	    	<icon size="lg" name="search"/>
+	    	<small class="text-align-center">Search</small>
+	    	<div class="indicator"></div>
+	    </router-link>
+	    <router-link to="/" class="flex-inline cursor-pointer text-grey flex-column align-items-center">
+	    	<icon size="lg" name="notification"/>
+	    	<small class="text-align-center">Notification</small>
+	    	<div class="indicator"></div>
+	    </router-link>
+	    <router-link to="/" class="flex-inline cursor-pointer text-grey flex-column align-items-center">
+	    	<icon size="lg" name="leader"/>
+	    	<small class="text-align-center">Leaderboard</small>
+	    	<div class="indicator"></div>
+	    </router-link>
+	    <router-link to="/" class="flex-inline cursor-pointer text-grey flex-column align-items-center">
+	    	<icon size="lg" name="course"/>
+	    	<small class="text-align-center">Course</small>
+	    	<div class="indicator"></div>
+	    </router-link>
+	  </div>
 </template>
 <script>
 export default {
@@ -16,14 +36,26 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-svg {
-	fill: #999 !important;
-	path {
-		fill: #999;
-
-	}
-	&:hover {
-		cursor: pointer;
-	}
+.indicator {
+	border: 1px solid color(bv-primary);
+	width: 100%;
+	margin-top: 10px;
 }
+a {
+	text-decoration: none;
+}
+small {
+	line-height: 0.3;
+	font-size: 10px;
+}
+// svg {
+// 	fill: #999 !important;
+// 	path {
+// 		fill: #999;
+
+// 	}
+// 	&:hover {
+// 		cursor: pointer;
+// 	}
+// }
 </style>
