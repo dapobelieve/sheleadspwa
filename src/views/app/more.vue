ebb9ff0ad84e42aca232218e4b4bd89ffeb13031
BenjaminAlamu <oluwaseunalamu@gmail.com>
2020-05-01 16:18:47 +0100

    Fixed More section

--- src/views/app/more.vue
+++ src/views/app/more.vue
@@ -1,53 +1,53 @@
 <template>
   <div class="more d-flex flex-column ml-8 mt-24">
-    <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+    <div class="menu-item d-flex">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="survey" />
         <span class="text-grey-900">Survey</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="poll" /> <span class="text-grey-900">Polls</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="announce" />
         <span class="text-grey-900">Announcement</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="event" />
         <span class="text-grey-900">Events</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" :to="{ name: 'leader' }">
         <icon size="lg" name="leader-menu" />
         <span class="text-grey-900">Leaderboard</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="resource" />
         <span class="text-grey-900">Resources</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="help" /> <span class="text-grey-900">Help</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link :to="{ name: 'profile' }">
+      <router-link class="d-flex align-items-center" :to="{ name: 'profile' }">
         <icon size="lg" name="settings" />
         <span class="text-grey-900">Settings</span>
       </router-link>
     </div>
     <div class="menu-item d-flex align-items-center">
-      <router-link to="#">
+      <router-link class="d-flex align-items-center" to="#">
         <icon size="lg" name="logout" />
         <span class="text-grey-900">Logout</span>
       </router-link>
