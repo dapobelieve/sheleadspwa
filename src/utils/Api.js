1784966bc3d955201d614f87465ecb692309a582
Dapo Michaels <dapomichaels@gmail.com>
2020-06-12 16:31:43 +0100

    remove quotes

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "@/store";
 
 const instance = axios.create({
-  baseURL: "process.env.VUE_APP_API"
+  baseURL: process.env.VUE_APP_API
 });
 
 // request interceptor
