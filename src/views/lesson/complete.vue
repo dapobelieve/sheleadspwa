3cf39a75fed42f211bfc711aa801a20c90f909c5
Benjamin Alamu <oluwaseunalamu@gmail.com>
2020-06-25 21:59:45 +0100

    More fixes

--- src/views/lesson/complete.vue
+++ src/views/lesson/complete.vue
@@ -3,14 +3,14 @@
     <top heading="Course Completed" />
     <div class="d-flex flex-column justify-content-center align-items-center" style="height: calc(100vh - 100px)">
       <div>
-        <img src="https://res.cloudinary.com/sla-sheleadsafrica/image/upload/v1591870657/assets/Vector_ms3gdr.png" alt="" />
+        <img src="https://res.cloudinary.com/sla-sheleadsafrica/image/upload/v1593117440/assets/complete-2_tlyuwa.gif" alt="" />
       </div>
       <div class="d-flex flex-column justify-content-center align-items-center mt-24">
         <span class="text-bolder">Congratulations!</span>
         <span class="text-bolder">You have completed the course</span>
       </div>
       <div class="d-flex flex-column">
-        <sla-button @click="$router.push({ name: 'leader' })" text="Go to leaderboard" class="mb-24 mt-24" />
+        <sla-button @click="$router.push({ name: 'leaderboard' })" text="Go to leaderboard" class="mb-24 mt-24" />
         <sla-button @click="$router.push({ name: 'all-survey' })" type="outline" text="Please fill surveys" />
       </div>
       <div class="mt-56">
