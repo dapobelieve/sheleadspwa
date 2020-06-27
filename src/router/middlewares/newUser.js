d1c91f34d0cd16486db0e8d36eb2d83a04f8ac86
Benjamin Alamu <oluwaseunalamu@gmail.com>
2020-06-27 02:52:13 +0100

    Fixed this Dapo's stubborn 3 month bug that he has been blaming me for. Alas it was him that pushed beans. I will still break his head

--- src/router/middlewares/newUser.js
+++ src/router/middlewares/newUser.js
@@ -1,22 +1,14 @@
 export default function({ store, next, router }) {
-  if (
-    typeof store.state.user.data.business_location == "undefined" ||
-    store.state.user.data.business_location.length == 0
-  ) {
+  console.log(store.state.user);
+  if (typeof store.state.user.data.business_bio == "undefined" || store.state.user.data.business_bio.length == 0) {
     router.replace({
       name: "info"
     });
-  } else if (
-    typeof store.state.user.data.intrests != "string" ||
-    store.state.user.data.intrests == ""
-  ) {
+  } else if (typeof store.state.user.data.intrests != "string" || store.state.user.data.intrests == "") {
     router.replace({
       name: "interests"
     });
-  } else if (
-    !store.state.user.data.industry ||
-    store.state.user.data.industry == ""
-  ) {
+  } else if (!store.state.user.data.industry || store.state.user.data.industry == "") {
     router.replace({
       name: "industry"
     });
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "@/store";
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API
+  baseURL: "http://localhost:5839/api/v1"
 });
 
 // request interceptor
