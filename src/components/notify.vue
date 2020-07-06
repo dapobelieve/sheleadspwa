8099d049a6d387d8cbb2c8a0f7aa1d57c34918f2
Dapo Michaels <dapomichaels@gmail.com>
2020-07-06 15:09:46 +0100

    fixes

--- src/components/notify.vue
+++ src/components/notify.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="d-flex justify-content-between align-items-center px-4 py-8">
     <sla-avatar size="lg" :user="{ image: 'https://sla-be.herokuapp.com/api/v1/files/0TXG1NJMMgtqunAq1emE.null/get' }" />
-    <div class="ml-12 flex-inline mr-4">
-      <span class="truncate truncate-2">{{ data.message }}</span>
-      <small style="color: #999999; font-size: 10px">{{ data.createdAt | chatTime }}</small>
+    <div class="ml-12 flex flex-column flex-inline mr-4">
+      <div class="truncate truncate-2">{{ data.message }}</div>
+      <small class="text-right" style="color: #999999; font-size: 8px">{{ data.createdAt | chatTime }}</small>
     </div>
     <div class="left-image">
       <img class="object-cover" :src="data.image" alt="" />
